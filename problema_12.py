8d97329f499b5f5f6f5c9e588a0fd4bc45c10562
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-10-01 17:41:22 +0300

    Add files via upload

---
+++ problema_12.py
@@ -0,0 +1,8 @@
+a=int(input("nr de oua de la prima gaina="))
+b=int(input("nr de oua de la a doua gaina "))
+c=int(input("nr de oua de la a treia gaina "))
+d=int(input("nr de oua de la a patra gaina "))
+s=(a+b+c+d)*7
+print(s,"oua se obtin intr-o saptamana")
+#Într-o  gospodărie  sunt  4  găini.  Introduceţi  în  calculator  prin  variabilele  a,  b,  c,  d numărul de ouă pe care-l dă fiecare găină într-o  zi.
+# Afişaţi câte ouă se obţin într-o săptămână.
